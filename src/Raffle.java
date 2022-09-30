6816b63180d36d4bf57221a4965204813efa0db9
Peter Hildorf <s224274@student.dtu.dk>
2022-09-30 16:50:58 +0200

    opdatering af spillet med 2 ens feature

--- src/Raffle.java
+++ src/Raffle.java
@@ -13,13 +13,14 @@ public class Raffle {
         this.t1 = new Terning();
         this.t2 = new Terning();
     }
-
-    public void roll(){
-        t1.roll();
-        t2.roll();
+    public boolean roll(){
+        int t1roll = t1.roll();
+        int t2roll = t2.roll();
         System.out.println(t1);
         System.out.println(t2);
+
         pointSum += t1.getFaceValue() + t2.getFaceValue();
 
+        return (t1roll == t2roll) ? true : false;
     }
 }
--- src/main.java
+++ src/main.java
@@ -7,24 +7,38 @@ public class main {
         Player player = new Player();
         Player player2 = new Player();
         int P1point = player.getPoint(), P2point = player2.getPoint();
-        boolean P1point1;
-
+        boolean isP1 = false;
+        boolean isPair = false;
 
         while (P1point <= 40 && P2point <= 40) {
-        System.out.println("Spiller1 slår:");
-        raffle.roll();
-        System.out.println("Spiller1 får summen:");
-        P1point = (player.getPoint() + raffle.getPointSum());
-        System.out.println(P1point);
+            isPair = false;
+            if(isP1){
+                System.out.println("Spiller1 slår:");
 
+                isPair = raffle.roll();
 
+                System.out.println("Spiller1 får summen:");
+                P1point = (player.getPoint() + raffle.getPointSum());
+                System.out.println(P1point);
+                if(isPair) {
+                    System.out.println("spiller ");
+                    continue;
+                }
 
-        System.out.println("Spiller2 slår:");
-        raffle2.roll();
-        System.out.println("Spiller2 får summen:");
-        P2point = (player2.getPoint() + raffle2.getPointSum());
-        System.out.println(P2point);
+            }else {
+                System.out.println("Spiller2 slår:");
+
+                isPair = raffle2.roll();
+
+                System.out.println("Spiller2 får summen:");
+                P2point = (player2.getPoint() + raffle2.getPointSum());
+                System.out.println(P2point);
+                if(isPair)continue;
+            }
+            isP1 = !isP1;
+
         }
+        System.out.println(P1point >= 40 ? "Spiller 1 vinder" : "Spiller 2 vinder");
 
 
     }
